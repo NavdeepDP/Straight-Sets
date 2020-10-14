2302513f908c3315b3868fa47ab3a77a442a1bfe
NP <navdeep.puri@gmail.com>
2020-10-14 15:18:07 -0400

    modified code to retrieve feedback with associated coach and player

--- controllers/feedbackPostController.js
+++ controllers/feedbackPostController.js
@@ -8,19 +8,95 @@ const db = require("../models");
 router.get("/post/:id", function (req, res) {
     console.log("new post");
     res.render("new-post");
-  });
+});
 
+router.get("/posts/:id", function (req, res) {
+    db.FeedbackPost.findAll({
+        where: {
+            PlayerId: req.params.id
+        }
+    })
+        .then((posts) => {
+            res.render("feedBackView", posts);
 
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to retrieve player feedback posts.",
+            });
+
+        });
+
+});
 
 // api ROUTES
 // =============================================================
-  // POST route for saving a new post
-  router.post("/api/posts", function(req, res) {
-      console.log("adding post");
-    db.FeedbackPost.create(req.body).then(function(dbPost) {
-      res.json(dbPost);
+// POST route for saving a new post
+router.get("/api/posts/:id", function (req, res) {
+    console.log("Getting post " + req.params.id);
+
+    db.FeedbackPost.findAll({
+        where: {
+            PlayerId: req.params.id,
+            
+        },
+        include: [db.Coach, db.Player]
+        
+           
+    })
+        .then((posts) => {
+            res.json({
+                error: false,
+                data: posts,
+                message: "Feeback List",
+            });
+
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to retrieve player feedback posts.",
+            });
+
+        });
+
+});
+
+router.post("/api/posts", function (req, res) {
+    console.log("adding post");
+    db.FeedbackPost.create(req.body).then(function (dbPost) {
+        res.json(dbPost);
     });
-  });
+});
+
+
+
+
+router.get("/api/posts", function (req, res) {
+    console.log("adding post");
+    db.FeedbackPost.findAll()
+        .then((posts) => {
+            res.json({
+                error: false,
+                data: posts,
+                message: "Feeback List",
+            });
+
+        })
+        .catch((err) => {
+            res.status(500).json({
+                error: true,
+                data: null,
+                message: "Unable to retrieve player feedback posts.",
+            });
+
+        });
+});
+
+
 
 
 module.exports = router;
